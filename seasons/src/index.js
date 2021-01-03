b377d8f76486159d56ade4700ad28538416bc88b
saran-23 <romansaran446@gmail.com>
2021-01-03 09:39:11 +0530

    displaying seasonDisplay Component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+    state = { lat: null, errorMessage: '' };
 
-        this.state = { lat: null, errorMessage: '' };
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({ lat: position.coords.latitude })
-            },
-            err => {
-                this.setState({ errorMessage: err.message })
-            }
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
         );
     }
+
     render() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>;
         }
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude:{this.state.lat}</div>;
+            return <SeasonDisplay  lat = {this.state.lat}/>
         }
         // if (!this.state.errorMessage && !this.state.lat) {
         //     return <div>Loading</div>
