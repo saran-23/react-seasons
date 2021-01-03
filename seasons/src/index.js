e82159bf6262baa8d519b6830dcc810483d1e468
saran-23 <romansaran446@gmail.com>
2021-01-03 09:23:37 +0530

    workin with geolocation api

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-                
+
 class App extends React.Component {
-    render() {
-            window.navigator.geolocation.getCurrentPosition(
-                (Position) => console.log(Position),
-                err => console.log(err)
-            );
-        return <div>Latitude: </div>;
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null, errorMessage: '' };
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({ lat: position.coords.latitude })
+            },
+            err => {
+                this.setState({ errorMessage: err.message })
+            }
+        );
     }
+    render() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude:{this.state.lat}</div>;
+        }
+        // if (!this.state.errorMessage && !this.state.lat) {
+        //     return <div>Loading</div>
+        // }
+
+        return <div>Loading</div>;
+
+    };
 }
 
-ReactDOM.render(<App />,document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
