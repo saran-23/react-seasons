247891531e7347c21398da793c27f2771c37b215
saran-23 <romansaran446@gmail.com>
2021-01-03 13:25:02 +0530

    ternary operator used

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -3,11 +3,12 @@ import React from 'react';
 
 const seasonConfig = {
     summer: {
-    text: "Let's hit the beach",
+    text: "Let's hit the beach!",
     iconName:'sun'
+
     },
     winter: {
-        text: 'it is cold',
+        text: ' Ouch,it is cold!',
         iconName: 'snowflake'
     }
 };
@@ -16,7 +17,7 @@ const getSeason = (lat,month) => {
     if(month>2 && month<9) {
       return  lat > 0 ? 'summer' : 'winter';
     } else {
-        return lat <0 ? 'winter' : 'summer';
+        return lat < 0 ? 'winter' : 'summer';
     }
 }
 
@@ -24,7 +25,7 @@ const  SeasonDisplay = (props) => {
     const season = getSeason(props.lat,new Date().getMonth());
     const { text,iconName} = seasonConfig[season];
     return (
-            <div className={`season-display $(season)`}>
+            <div className={`season-display ${season}`}>
                 <i className={`icon-left massive ${iconName} icon`} />
                 <h1>{text}</h1>
                 <i className={`icon-right massive ${iconName} icon`} />
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import "semantic-ui-css/semantic.min.css";
-{/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" /> */}
 
 class App extends React.Component {
     state = { lat: null, errorMessage: '' };
